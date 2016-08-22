0f4109123c5ece3e8252ada5f6ef4b1fdcfce267
zzgo <531818100@qq.com>
2016-08-22 23:24:19 +0800

    +add file

---
+++ Objective-C/HZViewController.xctemplate/UIViewController/___FILEBASENAME___.m
@@ -0,0 +1,36 @@
+//
+//  Created by zzgo on 16/8/22.
+// 	Contact by zzgoCC@gmail.com
+//  Copyright © 2016年 ibireme. All rights reserved.
+//
+
+#import "___FILEBASENAME___.h"
+
+@interface ___FILEBASENAME___ ()
+
+@end
+
+@implementation ___FILEBASENAME___
+
+#pragma mark - Intial Methods
+
+#pragma mark - Override/Life Cycle
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+#pragma mark - Notification
+
+#pragma mark - KVO
+
+#pragma mark - target-action/IBActions
+
+#pragma mark - delegate dataSource protocol
+
+#pragma mark - public
+
+#pragma mark - private
+
+#pragma mark - getter / setter
+
+@end
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # CodeTemplate
 包含代码模板，以及自定义clang-format的格式文件
+
+xcode7下，前往文件夹
+
+>/Applications/Xcode.app/Contents/Developer/Library/Xcode/Templates/File Templates/Source
+
+添加代码模板文件夹，就可以使用
+
+.clang-format文件，放在iOS项目的根目录下使用。
